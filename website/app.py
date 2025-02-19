fab686b7ae1a7775d080186255037746227e3201
Yogeshwar-CM <cmyogeshwar@gmail.com>
2025-02-20 02:23:40 +0530

    UI Frontend

--- website/app.py
+++ website/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to the Flask App!"
+    return render_template('index.html')
 
 @app.route('/api', methods=['POST'])
 def api():
